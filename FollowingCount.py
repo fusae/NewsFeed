cc42fc1133a6061768040f89ee8a728f1627c785
fusae <fusae_js@163.com>
2023-01-29 22:38:04 +0800

    添加md5加密，避免重复存储

--- FollowingCount.py
+++ FollowingCount.py
@@ -3,11 +3,16 @@ from collections import Counter
 from TwitterBot import TwitterBot
 import os
 import json
+import hashlib
 
 DIR_PATH = os.getcwd()
 CONFIG_NAME = os.path.join(DIR_PATH, "WeChat_Config.json")
 DATA_DIR = os.path.join(DIR_PATH, "Data")
-TWITTER_DATA_DIR = os.path.join(DATA_DIR, "Twitter") 
+TWITTER_DATA_DIR = os.path.join(DATA_DIR, "Twitter")
+FOLLOWING_COUNT_DIR = os.path.join(DATA_DIR, "Following_Count")
+
+if not os.path.exists(FOLLOWING_COUNT_DIR):
+	os.makedirs(FOLLOWING_COUNT_DIR)
 
 #read file to get user_KOL_following
 def get_user_KOL_following(userid):
@@ -31,6 +36,7 @@ def get_common_following(bot, userids):
 
         most_common = Counter(total_following).most_common()
 
+        md5_list = []
         for each in most_common:
             if each[1] > 1:
                 print(each[0])
@@ -45,6 +51,18 @@ def get_common_following(bot, userids):
                 }
 
                 bot.wechatpush.send_message(content)
+
+                # sign string as md5 string, and save it to local file, to avoid send repeatly
+                md5_string = hashlib.md5((handle+count).encode('utf-8')).hexdigest()
+                md5_list.append(md5_string)
+
+        data = {'md5': md5_list}
+        user_file = os.path.join(FOLLOWING_COUNT_DIR, userid + ".json")
+
+        with open(user_file, "w") as f:
+            data = json.dumps(data)
+            f.write(data)
+
 
 if __name__ == '__main__':
 
